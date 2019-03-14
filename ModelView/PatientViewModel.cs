b65c5bd9803ca4c8c2f0cbda71dd3927a1627223
kfir2037 <34978872+kfir2037@users.noreply.github.com>
2019-03-14 11:28:09 +0200

    Add files via upload

---
+++ ModelView/PatientViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MedicalStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicalStore.ModelView
+{
+    public class PatientViewModel
+    {
+        public Patient patient { get; set; }
+        public List<Patient> patients { get; set; }
+    }
+}
---
+++ ModelView/RequestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MedicalStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicalStore.ModelView
+{
+    public class RequestViewModel
+    {
+        public Request request { get; set; }
+        public List<Request> requests { get; set; }
+    }
+}
